6926831a798a12f45579efe067f7d928e9f885cb
PMC MALWARE CYBER <139885500+Unlimited-Crack-You@users.noreply.github.com>
2023-10-14 15:25:19 +0700

    Add files via upload

---
+++ spy_the_infor.sh
@@ -0,0 +1,140 @@
+#!/bn/bash
+
+# Script UCH 2009
+# Nomer Admin 085600685685
+# TOOLS FROM UCH 2009
+# CANNOT COPY CODE NGOTAK TOLOL 
+#KAYA GAK SEKOLAH AJING TOD
+#TOOLS VERSI 1.0 DI BUAT TANGGAL 25 AGUSTUS 2023
+#version 1.0
+
+# Color Code
+
+#SCRIPT FOR TERMUX
+
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+RED="$(printf '\033[31m')"  
+GREEN="$(printf '\033[32m')"  
+ORANGE="$(printf '\033[33m')"  
+BLUE="$(printf '\033[34m')"
+MAGENTA="$(printf '\033[35m')"  
+CYAN="$(printf '\033[36m')"  
+WHITE="$(printf '\033[37m')" 
+BLACK="$(printf '\033[30m')"
+REDBG="$(printf '\033[41m')"  
+GREENBG="$(printf '\033[42m')"  
+ORANGEBG="$(printf '\033[43m')"  
+BLUEBG="$(printf '\033[44m')"
+MAGENTABG="$(printf '\033[45m')"  
+CYANBG="$(printf '\033[46m')"  
+WHITEBG="$(printf '\033[47m')" 
+BLACKBG="$(printf '\033[40m')"
+DEFAULT_FG="$(printf '\033[39m')"  
+DEFAULT_BG="$(printf '\033[49m') "
+
+clear
+echo " "
+echo $RED"       ████████████      ${ORANGE}╔╦╗ ╦ ╦ ╔═╗   ╔═╗ ╔═╗ ╔═╗ ╦ ╦   ╦ ╔╗╔ ╔═╗ ╔═╗ ╦═╗       ${RED}████████████        "
+echo $WHITE"       ████████████      ${ORANGE} ║  ╠═╣ ║╣    ╚═╗ ╠═╝ ╠═╣ ╚╦╝   ║ ║║║ ╠╣  ║ ║ ╠╦╝       ${BLACK}████████████        "
+echo $WHITE"         INDONEISA       ${ORANGE} ╩  ╩ ╩ ╚═╝   ╚═╝ ╩   ╩ ╩  ╩    ╩ ╝╚╝ ╚   ╚═╝ ╩╚═       ${WHITE}BLACK COMUY  "
+
+echo "                         ${RED}_________________________________________________"					
+echo "                                  ${GREEN}Created by: /B7. ( Black Seven )   " 
+echo "      ${WHITE}--------------------------------------------------------------------------------------"				                   
+echo "        ${RED}Any actions and or activity related to The Spay infor is solely your responsibility"
+echo "      ${WHITE}--------------------------------------------------------------------------------------" 
+echo " "
+echo "    Author  : ./B7"
+echo "    GitHub  : https://github.com/Unlimited-Crack-You"
+echo "    Version : Versi 0.5"
+echo " "
+echo "   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒     ╭──────────────────────────────────────────────────╮"
+echo "   ▒▒▒▒▒▒▒▒▄▄▄▄▄▄▄▄▒▒▒▒▒▒     │${BLUE}[${ORANGE}F1${BLUE}]${WHITE} ==>  Install and Configure Requirements  <== │"
+echo "   ▒▒${RED}█${WHITE}▒▒▒▄██████████▄▒▒▒▒     │${BLUE}[${ORANGE}F2${BLUE}]${WHITE}         Web Page / Company Information       │"
+echo "   ▒${RED}█▐${WHITE}▒▒▒████████████▒▒▒▒     │${BLUE}[${ORANGE}F3${BLUE}]${WHITE}           Information of a Person            │" 
+echo "   ▒${RED}▌▐${WHITE}▒▒██▄▀██████▀▄██▒▒▒     │${BLUE}[${ORANGE}F4${BLUE}]${WHITE} Information of a Nickname / Alias / Nickname │" 
+echo "   ${RED}▐┼▐${WHITE}▒▒██▄▄▄▄██▄▄▄▄██▒▒▒     │${BLUE}[${ORANGE}F5${BLUE}]${WHITE}         Information of a Social Network      │"
+echo "   ${RED}▐┼▐${WHITE}▒▒██████████████▒▒▒     │${BLUE}[${ORANGE}F6${BLUE}]${WHITE}            Information of an Email           │" 
+echo "   ${RED}▐▄▐${WHITE}████─▀▐▐▀█─█─▌▐██▄▒     │${BLUE}[${ORANGE}F7${BLUE}]${WHITE}         Telephone Number Information         │" 
+echo "   ▒▒${RED}█${WHITE}████──────────▐███▌     │${BLUE}[${ORANGE}F8${BLUE}]${WHITE}          Information of a Public IP          │" 
+echo "   ▒▒${RED}█${WHITE}▀▀██▄█─▄───▐─▄███▀▒     │${BLUE}[${ORANGE}F9${BLUE}]${WHITE}           Information of an Image            │"
+echo "   ▒▒${RED}█${WHITE}▒▒███████▄██████▒▒▒     │${BLUE}[${ORANGE}KL${BLUE}]${WHITE}    ------------->  Exit"" <---------------     │"
+echo "   ▒▒▒▒▒██████████████▒▒▒     ╰──────────────────────────────────────────────────╯" 
+echo "   ▒▒▒▒▒█████████▐▌██▌▒▒▒     ╭──────────────────────────────────────────────────╮"
+echo "   ▒▒▒▒▒▒▒▒▄▄▄▄▄▄▄▄▒▒▒▒▒▒     │${BLUE}[${ORANGE}01${BLUE}]${WHITE}        TOOLS BY UC-09 FROM TOOLS V1.5        │"
+echo "   ▒▒▒▒▒▐▀▐▒▌▀█▀▒▐▒█▒▒▒▒▒     │${BLUE}[${ORANGE}02${BLUE}]${WHITE}           TOOLS SPAM BY UC-09 V0.1           │"
+echo "   ▒▒▒▒▒▒▒▒▒▒▒▐▒▒▒▒▌▒▒▒▒▒     │${BLUE}[${ORANGE}03${BLUE}]${WHITE}                 GRUP WHASTAPP                │"
+echo "   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒     ╰──────────────────────────────────────────────────╯" 
+echo " "
+read -p "[•] Choose an Number : " opc1
+	case $opc1 in
+			F1 )	bash requisitos/0.sh
+				;;
+			F2 )	bash requisitos/1.sh
+				;;
+			F3 )	bash requisitos/2.sh
+				;;
+			F4 )	bash requisitos/3.sh
+				;;
+			F5 )	bash requisitos/4.sh
+				;;
+			F6 )	bash requisitos/5.sh
+				;;    
+			F7 )	bash requisitos/6.sh
+				;;
+			F8 )	bash requisitos/7.sh
+				;;
+			F9 )	bash requisitos/8.sh
+				;;
+			KL )	exit
+				;;
+				01 )
+				clear
+				pkg upgrade && pkg update
+pkg install git
+pkg install python
+pkg install python2
+pkg install neofetch
+rm -rf Fromv1-2009-UCH
+git clone https://github.com/Unlimited-Crack-You/Fromv1-2009-UCH
+cd Fromv1-2009-UCH
+bash update.sh
+
+;;
+
+				02)
+				clear
+				pkg upgrade && pkg update
+pkg install git
+pkg install python
+pkg install python2
+pkg install neofetch
+rm -rf Spam-FromV1
+				git clone https://github.com/Unlimited-Crack-You/Spam-FromV1
+				cd Spam-FromV1
+				bash install.sh
+				;;
+				03 )
+				am start -a android.intent.action.VIEW https://chat.whatsapp.com/LqvqjLZdBgt34lxWRrvi5e
+				
+				;;
+			
+			* )	echo
+				echo ${ORANGE}"$RRPLY Not a valid Numer Bro !! "
+				sleep 2
+				bash spy_the_infor.sh
+	esac
